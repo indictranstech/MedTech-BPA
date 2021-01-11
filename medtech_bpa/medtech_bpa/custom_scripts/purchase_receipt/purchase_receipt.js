851b7c86f55335bb2e0094d5c099712243de3846
Indictrans <sukrut25j@gmail.com>
2021-01-11 11:51:04 +0530

    [FIX][Purchase Receipt]

--- medtech_bpa/medtech_bpa/custom_scripts/purchase_receipt/purchase_receipt.js
+++ medtech_bpa/medtech_bpa/custom_scripts/purchase_receipt/purchase_receipt.js
@@ -9,21 +9,25 @@ frappe.ui.form.on("Purchase Receipt", {
 frappe.ui.form.on("Purchase Receipt Item", {
 	physically_verified_quantity :function(frm,cdt,cdn){
 		var row = locals[cdt][cdn]
+		frappe.model.set_value(cdt, cdn, 'qty', 0);
+		frappe.model.set_value(cdt, cdn, 'short_quantity', 0);
+		frappe.model.set_value(cdt, cdn, 'excess_quantity', 0);
 		var short_quantity = 0.0
 		var excess_quantity = 0.0
 		var diff = flt(row.physically_verified_quantity - row.received_qty)
 		if (diff < 0){
-			row.short_quantity = Math.abs(diff)
+			// row.short_quantity = Math.abs(diff)
 			frappe.model.set_value(cdt, cdn, 'short_quantity', Math.abs(diff));
 			frm.refresh_field("short_quantity");
 		}
 		else if(diff >= 0){
-			row.excess_quantity = Math.abs(diff)
+			// row.excess_quantity = Math.abs(diff)
 			frappe.model.set_value(cdt, cdn, 'excess_quantity', Math.abs(diff));
 			frm.refresh_field("excess_quantity");
 		}
 		var accepted_qty = row.received_qty - short_quantity + excess_quantity - row.rejected_qty
-		frappe.model.set_value(cdt, cdn, 'accepted_qty', accepted_qty);
+		frappe.model.set_value(cdt, cdn, 'qty', accepted_qty);
 		frm.refresh_field("accepted_qty");
 	}
 });
+
--- medtech_bpa/medtech_bpa/custom_scripts/purchase_receipt/purchase_receipt.py
+++ medtech_bpa/medtech_bpa/custom_scripts/purchase_receipt/purchase_receipt.py
@@ -7,8 +7,13 @@ from frappe.utils import flt
 def validate(doc, method):
 	if doc.items:
 		for item in doc.items:
-			short_qty = 0 
+			item.qty = 0
+			short_qty = 0
+			item.short_quantity = 0
 			excess_qty = 0
+			item.excess_quantity = 0
+			item.rejected_qty = 0
+
 			if item.quality_inspection:
 				rejected_quantity = frappe.db.get_value("Quality Inspection", item.quality_inspection, "rejected_quantity")
 				item.rejected_qty = rejected_quantity
@@ -19,7 +24,7 @@ def validate(doc, method):
 				item.short_quantity =  abs(short_qty)
 				diff_for_excess_qty = diff if diff >= 0 else 0
 				item.excess_quantity = diff_for_excess_qty
-				accepted_qty = item.received_qty - abs(short_qty) + excess_qty - item.rejected_qty
+				accepted_qty = item.received_qty - abs(item.short_quantity) + item.excess_quantity - item.rejected_qty
 				# accepted_qty = item.received_qty -  item.rejected_qty
 				item.qty = accepted_qty
 			
