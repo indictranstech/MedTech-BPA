a546ba70092d998a759d9a835bd1a7cd7d66f7d1
AnilPise <anil.p@indictrans.in>
2021-01-15 03:00:05 +0530

    [Feature][Custom Report][Visual Inspection Report created]

---
+++ medtech_bpa/medtech_bpa/report/visual_inspection_report/visual_inspection_report.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2016, Indictrans and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Visual Inspection Report"] = {
+	"filters": [
+		{
+			"label": __("From Date"),
+			"fieldname":"from_date",
+			"fieldtype": "Date"
+		},
+		{
+			"label": __("To Date"),
+			"fieldname":"to_date",
+			"fieldtype": "Date"
+		}
+	]
+};
