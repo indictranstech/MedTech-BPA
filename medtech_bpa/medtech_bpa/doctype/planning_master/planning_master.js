0db29d5f855117617b61fc86de3e1a9ec2dc2d4e
Yugandhara <yugandhara.b@indictranstech.com>
2021-01-21 12:15:38 +0530

    [Feature][Doctype Planning master, planning master item and medtech settings created]

---
+++ medtech_bpa/medtech_bpa/doctype/planning_master/planning_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Indictrans and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Planning Master', {
+	// refresh: function(frm) {
+
+	// }
+});
