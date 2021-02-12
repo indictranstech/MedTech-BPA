6dba436dfa56299b9c586c3ca9cdadeafd2fefb2
Yugandhara <yugandhara.b@indictranstech.com>
2021-02-12 15:21:20 +0530

    [Feature][Planning screen][By deepak pawar]

--- medtech_bpa/medtech_bpa/page/planning_screen/planning_screen.js
+++ medtech_bpa/medtech_bpa/page/planning_screen/planning_screen.js
@@ -1,82 +1,431 @@
 frappe.pages['planning-screen'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Planning Screen',
-		single_column: true
-	});
-	controller = new frappe.planning_screen(wrapper);
+        var page = frappe.ui.make_app_page({
+    parent: wrapper,
+    title: 'Planning Screen',
+    single_column: true
+  });
+  controller = new frappe.planning_screen(wrapper);
 }
 
 frappe.planning_screen = Class.extend({
-	init :function(wrapper){
-		var me = this;
-		me.wrapper_page = wrapper.page
-		// '.layout-main-section-wrapper' class for blank dashboard page
-		this.page = $(wrapper).find('.layout-main-section-wrapper');
-		$(frappe.render_template('planning_screen_html')).appendTo(this.page);
+  init :function(wrapper){
+    
+                var me = this;
+                var update_values=[]
+                me.render_list=[]
+                me.update_values = update_values
+    me.wrapper_page = wrapper.page
+                me.delete_name = ''
+    // '.layout-main-section-wrapper' class for blank dashboard page
+    this.page = $(wrapper).find('.layout-main-section-wrapper');
+    $(frappe.render_template('planning_screen_html')).appendTo(this.page);
+                
 
-		$(".create_new").click(function(){
-			$('.planning_screen').html($(frappe.render_template('create_new')));
-			me.new_from_date()
-			me.new_to_date()
 
-			$(".create").click(function(){
-				me.get_data()
-			});
-		});
-	},
-	get_data:function(){
-		var me= this;
-		frappe.call({
-				method: "medtech_bpa.medtech_bpa.page.planning_screen.planning_screen.get_items_data",
-				async: false,
-				freeze_message:"Loading ...Please Wait",
-				args:{
-					from_date : me.new_project_from_date,
-					to_date : me.new_project_to_date
-				},
-				callback: function(r) {
-					data=r.message
-					$('.create_new_table').html($(frappe.render_template('create_new_table'),{"data":data}));
-				}
-		});
-	},
-	new_from_date:function(){
-		var me= this;
-		var new_from_date = frappe.ui.form.make_control({
-		    parent: this.page.find(".new_from_date"),
-		    df: {
-				label: '<b>From Date</b>',
-				fieldtype: "Date",
-				options: "",
-				fieldname: "",
-				placeholder: __("From Date"),
-				change:function(){
-					$("#new_from_date").val(new_from_date.get_value())
-					me.new_project_from_date = new_from_date.get_value()
-				}
-	     	},
-	     	only_input: false,
-   		});
-		new_from_date.refresh();
-	},
-	new_to_date:function(){
-		var me= this;
-		var new_to_date = frappe.ui.form.make_control({
-		    parent: this.page.find(".new_to_date"),
-		    df: {
-				label: '<b>To Date</b>',
-				fieldtype: "Date",
-				options: "",
-				fieldname: "",
-				placeholder: __("To Date"),
-				change:function(){
-					$("#new_to_date").val(new_to_date.get_value())
-					me.new_project_to_date = new_to_date.get_value()
-				}
-	     	},
-	     	only_input: false,
-   		});
-		new_to_date.refresh();
-	},
+                $(".update_view").click(function(){
+                  $('.planning_screen').html($(frappe.render_template('create_new')));
+                  $('label[for="from_date"]').hide()
+                  $('label[for="to_date"]').hide()
+                  $('.new_from_date').hide()
+                  $('.new_to_date').hide()
+                  $('.create').hide()
+                  $('.save_new').hide()
+                  $('.planning_master').show()
+                  $("label[for=planning_master]").css({'display': 'inline-block','width': '90%','text-align': 'right'})
+                  me.planning_master()
+
+
+       $(document).on('keydown', '.date_class' ,function(event)
+                {
+  var code = event.keyCode;
+  var key = event.key;
+    if (code== 32){
+  event.preventDefault();
+  }
+  if (!isNaN(Number(key)))
+    return;
+//&& (code < 48 || code > 57
+  // allow backspace, delete, left & right arrows, home, end keys
+  if (code == 8 || code == 46 || code == 37 || code == 39 || code == 36 || code == 35 || code == 190 || code ==110) {
+    return;
+  } else {
+    event.preventDefault();
+  }
+                //blocking keys that are not necessary
+              /*  if (e.which != 37 && e.which != 39 && e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57))
+                {
+                  $(this).html("Digits Only").show();
+                  $(this).html($(this).data().initial)
+                  $(this).css('opacity' , 1)
+                  return false;
+                }*/
+                });
+                
+                //collecting values to be updated in array
+                $(document).on('focusout', '.date_class' ,function()
+                {   
+                  if($(this)[0].innerHTML != $(this).data().initial)
+                {
+                 
+                 me.update_values.push([$(this).attr("class").split(' ')[1],$(this)[0].innerHTML,$(this).data().initial])
+                }
+                });
+                
+                $(".update").click(function(){
+                me.update_data()
+                })
+
+                $(".delete").click(function(){
+                if(me.delete_name != ""){
+                  frappe.confirm(
+                  "Are you sure you want to delete this entry ?",
+
+                  function(){
+
+                  frappe.call({
+                  method:"medtech_bpa.medtech_bpa.page.planning_screen.planning_screen.delete_data",
+                  async:false,
+                  args:{
+                    delete_data:me.planning_master1
+                  },
+                  callback: function(r){
+                  
+                  if (r.message=="1"){
+                  
+                  frappe.msgprint("Data Deleted Succesfully")
+                  window.location.reload();
+                  }
+                  }
+                  });
+                  },
+                  function(){
+                  //function if user selects no
+                  },
+
+                  )}
+                })
+                });
+
+
+
+    $(".create_new").click(function(){
+
+                        me.new_project_from_date = undefined
+                        me.new_project_to_date = undefined
+      $('.planning_screen').html($(frappe.render_template('create_new')));
+                        $('.planning_master').hide()
+                        $('.delete').hide()
+                        $('label[for="planning_master"]').hide()
+                        $('.update').hide()
+      me.new_from_date()
+      me.new_to_date()
+                        //me.planning_master()
+      
+      
+      $(".create").click(function(){      
+                          me.get_data();
+       $(document).on('keydown', '.date_class' ,function(event)
+                {
+  var code = event.keyCode;
+  var key = event.key;
+  if (code== 32){
+  event.preventDefault();
+  }
+  if (!isNaN(Number(key)))
+    return;
+//&& (code < 48 || code > 57
+  // allow backspace, delete, left & right arrows, home, end keys
+  if (code == 8 || code == 46 || code == 37 || code == 39 || code == 36 || code == 35 || code == 190 || code ==110) {
+    return;
+  } else {
+    event.preventDefault();
+  }
+                //blocking keys that are not necessary
+              /*  if (e.which != 37 && e.which != 39 && e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57))
+                {
+                  $(this).html("Digits Only").show();
+                  $(this).html($(this).data().initial)
+                  $(this).css('opacity' , 1)
+                  return false;
+                }*/
+                });
+                         //checking if data is written in first cell and then copying to rest of the cells on first write.
+                          $('.1').on ('focusout',function() 
+                           {
+                             var val = $(this).html()
+                             var myCol = $(this).index();
+                             var $tr = $(this).closest('tr');
+                             var myRow = $tr.index();
+                            if (Boolean(val))
+                            {
+
+                              $('tr').eq(myRow).find('.2').each(function(i,el)
+                              {
+
+                                if($(this).html()=='<br>')
+                              {
+
+                                $(this).html(val)
+                              }
+                              else{
+                              if($(this)[0].innerText==""){
+                              $(this)[0].innerText=val
+                              }  } 
+                            })
+                          }
+                          });
+
+$("td > input.bom").each(function(elem) {
+
+var input = document.getElementById(this.id);
+    input.awesomplete = new Awesomplete(input, {
+                                minChars: 0,
+              maxItems: 99,
+                autoFirst: true,
+                list:[]                });
+
+
+
+$("#" + this.id).on("awesomplete-selectcomplete", function(e){
+
+    var o = e.originalEvent;
+    var value=o.text.value
+    this.value=value;
+
+    });
+
+$("#" +this.id).on("focus",function(e){
+$(e.target).val('').trigger('input');
+
+})
+
+$("#" +this.id).on('input', function(e) {
+       var val = $(this).html()
+                             var myCol = $(this).index();
+                             var $tr = $(this).closest('tr');
+                             var myRow = $tr.index();
+      frappe.call({
+        method: "medtech_bpa.medtech_bpa.page.planning_screen.planning_screen.return_list",
+        args: {
+                                item_detail : $('tr').eq(myRow).find('#item_code').html()
+          },
+        callback: function(r) {
+            me.render_list.push([this.id,r.message])
+            e.target.awesomplete.list=r.message
+        }
+      });
+
+
+
+}) 
+
+
+
+
+})
+
+      });
+                            
+$(".save_new").click(function(){
+
+                if (me.new_project_from_date != undefined &&  me.new_project_to_date != undefined)
+                    {      $(this).hide()}
+                          me.save_data()
+                        });
+                    });
+
+        },
+
+
+update_data:function(){
+                  var me =this;
+                  if(me.delete_name != ""){
+                  frappe.call({
+                  method:"medtech_bpa.medtech_bpa.page.planning_screen.planning_screen.update_data",
+                  async:false,
+                  args:{
+                    update_data:me.update_values
+                  },
+                  callback: function(r){
+                  me.update_values.forEach(function(element){
+                  $('.'+element[0].split("/").join(`\\/`)).data('initial',(element[1] != '<br>') ? element[1] : 0)
+                  if (element[1]=='<br>'){
+                  $('.'+element[0].split("/").join(`\\/`)).html(0)}
+                  })
+                  me.update_values=[]
+                  if (r.message[0]== "0"){
+                  if (r.message.length== 2){
+                  frappe.msgprint(__(r.message[1]))
+                  }
+                  else
+                 { frappe.msgprint(__("Nothing to update"))}
+                  }
+                  if (r.message[0]=='1')
+                  {
+                  
+                    frappe.msgprint(__("Planning Master {0} updated successfully",[r.message[1]]))
+                  }
+                  
+                  }
+                  });}
+        },
+
+save_data:function(){
+                var me = this;
+                var  row_id=[]
+                var dict = {}
+                $("tr:first td").each(function(i,el){
+                  if (el.id != ''){  
+                  row_id.push(el.id)}
+                 })
+                 
+                 row_id.forEach(function(element){
+                  var values= [];
+                  if (element != "bom"){
+                  $('tr:not(:first-child) td[id*= '+element+'  ]').each(function(i,el){
+                    
+                      values.push(el.innerHTML)
+                      
+                  })}
+                  else{
+                  
+                   $('tr:not(:first-child) td').find('.bom').each(function(i,el){
+                  values.push(el.value)
+                  })
+                  }
+                      dict[element]=values
+                  })
+                  
+                if (me.new_project_from_date != undefined &&  me.new_project_to_date != undefined)
+                  {frappe.call({
+                  method:"medtech_bpa.medtech_bpa.page.planning_screen.planning_screen.save_items_data",
+                  async:false,
+                  args:{
+                    from_date:me.new_project_from_date,
+                    to_date:me.new_project_to_date,
+                    data:dict
+                  },
+                  callback: function(r){
+                  if (r.message[0]=='1'){ 
+                    $('.planning_master').show()
+                    $('.planning_master').html(r.message[1])
+                    $('.planning_master').css({'padding-top':"0px",
+                      'border': '1px solid #d1d8dd',
+                    'font-weight' :'bold',
+                    'text-align':'center',
+                    'width':'150px', 'padding-top': '3px','height':'30px','border-radius':'3px','top':'0'})
+                    $('label[for="planning_master"]').show()
+                  frappe.msgprint(__("Planning Master {0} is created.",[r.message[1]]))
+                  
+                  }
+                  
+                  }
+                  });}
+                
+        },
+
+
+get_data:function()
+        {
+    var me= this;
+                if (me.new_project_from_date != undefined &&  me.new_project_to_date != undefined){
+    frappe.call({
+        method: "medtech_bpa.medtech_bpa.page.planning_screen.planning_screen.get_items_data",
+        async: false,
+        freeze_message:"Loading ...Please Wait",
+        args:{
+          from_date : me.new_project_from_date,
+          to_date : me.new_project_to_date
+        },
+        callback: function(r) {
+          data=r.message
+          $('.create_new_table').html($(frappe.render_template('create_new_table'),{"data":data}));
+        }
+    });}
+  },
+
+        fetch_data : function(elem)
+        {
+          var me = this;
+          frappe.call({
+          method : "medtech_bpa.medtech_bpa.page.planning_screen.planning_screen.fetch_data",
+          async: false,
+          freeze_message : "Loading ... Please Wait",
+          args:{
+            name : elem
+
+          },
+          callback:function(r){
+          data=r.message
+          
+          $('.create_new_table').html($(frappe.render_template('create_new_table'),{"data":data}));
+          
+          }
+
+          });
+        },
+
+new_from_date:function()
+        {
+    var me= this;
+    var new_from_date = frappe.ui.form.make_control({
+        parent: this.page.find(".new_from_date"),
+        df: {
+        label: '<b>From Date</b>',
+        fieldtype: "Date",
+        options: "",
+        fieldname: "",
+        placeholder: __("From Date"),
+        change:function(){
+          $("#new_from_date").val(new_from_date.get_value())
+          me.new_project_from_date = new_from_date.get_value()
+        }
+        },
+        only_input: false,
+      });
+    new_from_date.refresh();
+  },
+  
+        new_to_date:function()
+        {
+    var me= this;
+    var new_to_date = frappe.ui.form.make_control({
+        parent: this.page.find(".new_to_date"),
+        df: {
+        label: '<b>To Date</b>',
+        fieldtype: "Date",
+        options: "",
+        fieldname: "",
+        placeholder: __("To Date"),
+        change:function(){
+          $("#new_to_date").val(new_to_date.get_value())
+          me.new_project_to_date = new_to_date.get_value()
+        }
+        },
+        only_input: false,
+      });
+    new_to_date.refresh();
+  },
+        
+planning_master:function()
+        {
+          var me=this;
+          var planning_master = frappe.ui.form.make_control({
+          parent: this.page.find(".planning_master"),
+          df:{
+          label:'<b>Planning Master</>',
+          fieldtype:'Link',
+          fieldname: 'planning_master',
+          options: 'Planning Master',
+          change:function(){
+          if( Boolean(planning_master.get_value())){
+          me.delete_name=planning_master.get_value();
+          me.planning_master1 = planning_master.get_value();
+          me.fetch_data(planning_master.get_value())}
+          }
+          },
+          only_input:false,
+        })
+        planning_master.refresh();
+        },
 })
--- medtech_bpa/medtech_bpa/page/planning_screen/planning_screen.py
+++ medtech_bpa/medtech_bpa/page/planning_screen/planning_screen.py
@@ -1,59 +1,162 @@
-# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+#Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: GNU General Public License v3. See license.txt
 from __future__ import unicode_literals
 import frappe
 
 from frappe.model.document import Document
-import json
-from frappe.utils import nowdate, cstr, flt, cint, now, getdate,get_datetime,time_diff_in_seconds,add_to_date,time_diff_in_seconds,add_days
+from frappe.utils import nowdate, cstr, flt, cint, now, getdate,get_datetime,time_diff_in_seconds,add_to_date,time_diff_in_seconds,add_days,today
 from datetime import datetime,date, timedelta
+from frappe.model.naming import make_autoname
 import time
-from collections import OrderedDict
-
 import json
 import pandas as pd
-import csv
-import openpyxl
 from frappe import _
-import time
-import io
-import numpy as np
-from io import BytesIO
-from frappe.utils import cint, flt
-from datetime import datetime
-from collections import OrderedDict
-from datetime import date, timedelta
+
 
 @frappe.whitelist()
-def get_items_data(from_date, to_date):
-	print(from_date, to_date, type(from_date), type(to_date))
-	sdate =  datetime.strptime(from_date, '%Y-%m-%d').date()
-	edate =  datetime.strptime(to_date, '%Y-%m-%d').date()
+def save_items_data(from_date,to_date,data):
+    dates_check(from_date,to_date)
+    date_range = pd.date_range(from_date,to_date)
+    date_range= [(i).strftime("%d-%m-%Y") for i in  date_range]
+    doc = frappe.new_doc("Planning Master")
+    doc.from_date=from_date
+    doc.to_date=to_date
+    data = json.loads(data)
+    doc.title="abc"
+    doc.save()
+    frappe.db.set_value("Planning Master", doc.name,'title',doc.name)
+    for j in date_range:
+        for i in range(len(data['item_code'])):
+            doc_child= frappe.new_doc('Planning Master Item')
+            doc_child.item_code = data['item_code'][i]
+            doc_child.item_name = data['item_code'][i]
+            doc_child.date =  datetime.strptime(j,"%d-%m-%Y")
+            try:
+                if '<br>' in data[j][i]:
+                    doc_child.amount = float(data[j][i][0:-4]) if data[j][i][0:-4] not in ['',None,'.'] else 0
+                else:
+                    doc_child.amount =  float(data[j][i]) 
+            except Exception as e:
+                frappe.throw("Error in saving data at column %s and row %s"%(j,i+1))
+            doc_child.uom = data["uom"][i]
+            doc_child.bom = data["bom"][i]
+            doc_child.planning_master_parent = doc.name
+            doc_child.save()
+            frappe.db.set_value("Planning Master Item", doc_child.name,'title',doc_child.name)
+    return ["1",doc.name] 
 
-	delta = edate - sdate
+def dates_check(from_date,to_date):    
+    if from_date <= today():
+        frappe.throw("From Date must be greater than today.")
+    if to_date <= today():
+        frappe.throw("To Date must be greater than today.")
+    if to_date < from_date:
+        frappe.throw("To Date must be greater than or equals to From Date.")
+    date_overlap = frappe.db.sql("select name,from_date,to_date from `tabPlanning Master` where '%s' between from_date and to_date or '%s' between from_date and to_date"%(from_date,to_date),as_list=1)
+    
+    if len(date_overlap)>0:
+        frappe.throw("Date overlaps with Planning Master %s having From Date %s and To Date %s "%(date_overlap[0][0],date_overlap[0][1].strftime('%d-%m-%Y'),date_overlap[0][2].strftime('%d-%m-%Y')))
 
-	date_list = []
+@frappe.whitelist()
+def delete_data(delete_data):
+    doc = frappe.get_doc("Planning Master",delete_data)
+    if doc.from_date <= date.today():
+        frappe.throw("The entry cannot be deleted because it contains past date or present date.")
+    names_of_child = [i[0] for i in frappe.db.sql("""select name from `tabPlanning Master Item` where planning_master_parent = '%s'"""%(delete_data))]
+    for i in names_of_child:
+        frappe.delete_doc("Planning Master Item",i)
 
-	for i in range(delta.days + 1):
-	    day = sdate + timedelta(days=i)
-	    print(day)
-	    date_list.append(day.strftime('%d-%m-%Y'))
-	data = dict()
-	data['header_list'] = date_list
+    frappe.delete_doc("Planning Master",delete_data)
+    frappe.db.commit()
+    return "1"
 
-	fg_item_group = frappe.db.sql("select item_group from `tabFG Item Group`", as_dict = 1)
-	if fg_item_group:
-		fg_group_list = ["'" + row.item_group + "'" for row in fg_item_group]
-		fg_item_group_list = ','.join(fg_group_list)
-	else:
-		fg_item_group_list = "' '"
+@frappe.whitelist()
+def get_items_data(from_date, to_date,listview=None):
+        dates_check(from_date,to_date)    
+        sdate =  datetime.strptime(from_date, '%Y-%m-%d').date()
+        edate =  datetime.strptime(to_date, '%Y-%m-%d').date()
+        delta = edate - sdate
+        date_list = []
+        date_dict= []
+        for i in range(delta.days + 1):
+            day = sdate + timedelta(days=i)
+        
+            if date.today() >= day:
+                date_dict.append([day.strftime('%d-%m-%Y'),0])
+            else:
+                date_dict.append([day.strftime('%d-%m-%Y'),1])
+            date_list.append(day.strftime('%d-%m-%Y'))
+    
+        data = dict()
+        data['header_list'] = date_dict
 
-	item_detail = frappe.db.sql("select i.item_code, i.item_group, i.stock_uom from `tabItem` i join `tabBOM` b on i.item_code = b.item where b.docstatus = 1 and i.item_group in ({0}) group by i.item_code order by i.item_group, i.item_code".format(fg_item_group_list), as_dict=1)
-	bom_query = frappe.db.sql("select b.item, b.name  from `tabBOM` b where b.docstatus = 1 and b.is_default = (select max(b2.is_default) from `tabBOM` b2 where b2.item = b.item and b.docstatus = 1)", as_dict = 1)
-	bom_dict = {bom.get('item') : bom.get('name') for bom in bom_query}
-	for row in item_detail:
-		row['bom'] = bom_dict.get(row.get('item_code'))
+        fg_item_group = frappe.db.sql("select item_group from `tabFG Item Group`", as_dict = 1)
+    
+        if fg_item_group:
+            fg_group_list = ["'" + row.item_group + "'" for row in fg_item_group]
+            fg_item_group_list = ','.join(fg_group_list)
+        else:
+            fg_item_group_list = "' '"
 
-	data['item_data'] = item_detail
+        item_detail = frappe.db.sql("select i.item_code, i.item_group, i.stock_uom from `tabItem` i join `tabBOM` b on i.item_code = b.item where b.docstatus = 1 and i.item_group in ({0}) group by i.item_code order by i.item_group, i.item_code".format(fg_item_group_list), as_dict=1)
+        bom_query = frappe.db.sql("select b.item, b.name  from `tabBOM` b where b.docstatus = 1 and b.is_default = (select max(b2.is_default) from `tabBOM` b2 where b2.item = b.item and b.docstatus = 1)", as_dict = 1)
+        bom_dict = {bom.get('item') : bom.get('name') for bom in bom_query}
+        for row in item_detail:
+            row['bom'] = bom_dict.get(row.get('item_code'))
+        data['item_data'] = item_detail
+        data['update']=0
+        return data
 
-	return data
+@frappe.whitelist()
+def fetch_data(name):
+    try:
+        send_data={}
+        date_range = pd.date_range(frappe.get_value('Planning Master',name,'from_date'),frappe.get_value('Planning Master',name,'to_date'))
+        date_range= [[(i).strftime("%d-%m-%Y"),0 if pd.Timestamp(date.today()) >= i else 1] for i in  date_range]
+        data = frappe.db.sql("""select GROUP_CONCAT(name) as name,item_code , uom as stock_uom,bom ,GROUP_CONCAT(amount) as amount from `tabPlanning Master Item` where planning_master_parent = '%s' group by item_code order by date"""%(name),as_dict=1)
+        for i in data:
+            i['name'] = i['name'].split(',')
+            i['amount']= [float(i) for i in i['amount'].split(',')]
+        send_data['header_list']=date_range
+        send_data['item_data']=data
+        send_data['update']=1
+        return send_data
+    except Exception as e:
+        error_log= frappe.log_error(message=frappe.get_traceback(), title=str(e))
+        frappe.throw("Error in fetching data {}".format(frappe.utils.get_link_to_form("Error Log",error_log.name)))
+
+@frappe.whitelist()
+def update_data(update_data):
+    try :
+
+        update_data = json.loads(update_data)
+        if  len(update_data) ==0:
+            return ["0"]
+        #update_data = json.loads(update_data)
+        for i in update_data:
+            for values in range(len(i)):
+                if type(i[values]) == str:
+                    if '<br>' in i[values]:
+                        i[values]=i[values][0:-4]
+                        i[values] = 0 if i[values] == '' else i[values]
+
+            doc = frappe.get_doc("Planning Master Item" ,i[0])
+            #if doc.amount== int(i[2]):
+            try:
+                doc.amount=float(i[1])
+            except Exception as e:
+                return ["0","Please check if the values changed  are correct"]
+            doc.save()
+            #else:
+                #frappe.throw("The old amount does not match for planning master item %s and amount %s"%(update_data[1],int(i[2])))
+        return ["1",doc.planning_master_parent]
+    except Exception as e:
+        error_log= frappe.log_error(message=frappe.get_traceback(), title=str(e))
+        frappe.throw("Error in updating data {}".format(frappe.utils.get_link_to_form("Error Log",error_log.name)))
+        
+        
+@frappe.whitelist()
+def return_list(item_detail):
+    item_detail = [i[0] for i in frappe.db.sql("select b.name from `tabItem` i join `tabBOM` b on i.item_code = b.item where i.item_code='%s'"%(item_detail), as_list=1)]
+    
+    return item_detail
