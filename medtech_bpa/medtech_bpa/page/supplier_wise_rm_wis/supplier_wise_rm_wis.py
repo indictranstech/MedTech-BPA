d4ec56ac268efe74d5d38016914285a16955290e
indictrans <jitendra.r@indictranstech.com>
2021-02-19 18:51:53 +0530

    [Fix- Report Supplier wise RM wise Pending PO against Shortage Report]

--- medtech_bpa/medtech_bpa/page/supplier_wise_rm_wis/supplier_wise_rm_wis.py
+++ medtech_bpa/medtech_bpa/page/supplier_wise_rm_wis/supplier_wise_rm_wis.py
@@ -7,6 +7,9 @@ import datetime
 import calendar
 import json
 import time
+from erpnext.stock.stock_ledger import get_previous_sle
+import operator
+import itertools
 
 @frappe.whitelist()
 def get_planing_master_details(filters=None):
@@ -15,65 +18,110 @@ def get_planing_master_details(filters=None):
 	from_date = datetime.date.today()
 	planning_master=frappe.db.sql("""SELECT name, from_date, to_date from `tabPlanning Master` where {0} """.format(get_filters_codition(filters)), as_dict=1)
 
-	bom_name = frappe.db.sql("""SELECT name, bom, amount from `tabPlanning Master Item` where planning_master_parent='{0}' and date>='{1}' and date<='{2}'""".format(planning_master[0].get('name'), from_date, planning_master[0].get('to_date')), as_dict=1)
+	bom_name = frappe.db.sql("""SELECT name, bom, amount from `tabPlanning Master Item` where planning_master_parent='{0}' and date>='{1}' and date<='{2}'""".format(planning_master[0].get('name'), planning_master[0].get('from_date'), planning_master[0].get('to_date')), as_dict=1)
 
+	unic_bom = []
 	bom_items=[]
 	for row in bom_name:
-		bom_data = frappe.db.sql("""SELECT a.name, a.quantity, b.qty, b.item_code, b.item_name, b.bom_no from `tabBOM` a join `tabBOM Item` b on a.name=b.parent where a.name='{0}'""".format(row.bom), as_dict=1)
+		if row.bom not in unic_bom:
+			unic_bom.append(row.bom)
+		bom_data = frappe.db.sql("""SELECT a.name, a.quantity, b.item_code, b.item_name, b.stock_qty from `tabBOM` a join `tabBOM Explosion Item` b on a.name=b.parent where a.name='{0}'""".format(row.bom), as_dict=1)
 		
 		for bom in bom_data:
 			bom['planning_master_item'] = row.name
 			bom_items.append(bom)
 
-	for bom_item in bom_items:
-		data.append(bom_item)
-		bom_childs =[]
-		lines = get_child_boms(bom_item,bom_childs)
-		for l in lines:
-			l['quantity'] = frappe.db.get_value("BOM", {'name':l.parent}, 'quantity')
-			data.append(l)
 
+	#planing_qty calculation from bom and planning master item
 	planing_qty=0.0
 	for plan_qty in bom_name:
-		for row in data:
+		for row in bom_items:
 			if row.planning_master_item==plan_qty.name:
-				planing_qty=flt(row.qty)/flt(row.quantity)*flt(plan_qty.amount)
+				planing_qty=flt(row.stock_qty)/flt(row.quantity)*flt(plan_qty.amount)
 				row["planing_qty"]=planing_qty
 
+	#group_by planning qty base on item_code
 	values_by_item = {}
-	for d in data:
+	for d in bom_items:
 		values_by_item.setdefault(d['item_code'], []).append(int(d['planing_qty']))
+	new_data=[{'item_code':k, 'planing_qty':sum(v)} for k, v in values_by_item.items()]
 
-	new_data=[{'item_code':m, 'planing_qty':sum(vs)} for m, vs in values_by_item.items()]
 	
+	warehouse = frappe.db.sql("""SELECT warehouse from `tabFG Warehouse Group` where parent='MedTech Settings'""", as_dict=1)
+
+	posting_time = nowtime()
 	for row in new_data:
 		row['from_date'] = from_date
 		row['to_date'] = planning_master[0].get('to_date')
 		row['po_qty']=0.0
 		row['consider_po_qty']=0.0
-	
+		row['current_stock'] = 0.0
 
+		#current stock calculation of item
+		for wh in warehouse:
+			last_entry = get_previous_sle({
+				"item_code": row.get('item_code'),
+				"warehouse" : wh.warehouse,
+				"posting_date": from_date,
+				"posting_time": posting_time })
+			row['current_stock'] += last_entry.qty_after_transaction if last_entry else 0.0
+
+	#calculate pending po qty
 	po_data = []
 	for row in new_data:
-		
-		actual_qty = frappe.db.sql("""select sum(actual_qty) as qty from tabBin
-		where item_code='{0}' """.format(row.get('item_code')), as_dict=1)
-		row['current_stock'] = actual_qty[0].get("qty")
-		print(actual_qty, row.get('item_code'))
 		po_details = frappe.db.sql("""SELECT a.name, a.supplier, b.item_code, b.qty from `tabPurchase Order` a join `tabPurchase Order Item` b on a.name=b.parent where a.docstatus=1 and a.schedule_date<='{0}' and b.item_code='{1}'""".format(row.get('to_date'), row.get('item_code')), as_dict=1)
 
 		for po in po_details:
-			if not frappe.db.get_value("Purchase Receipt Item", {'purchase_order':po.get('name')}, 'purchase_order'):
-				po_data.append(po)
+			accept_qty = frappe.db.get_values("Purchase Receipt Item", {'purchase_order':po.get('name'), 'item_code':po.get('item_code')}, ['item_code', 'actual_accepted_qty', 'parent', 'purchase_order'], as_dict=1)
+			if accept_qty and po.get('item_code') == accept_qty[0].get('item_code'):
+				po['qty'] = po.get('qty')-accept_qty[0].get('actual_accepted_qty')
+
+			po_data.append(po)
+
+	#calculate pending po qty for supplier
 	supplier = []
 	for row in new_data:			
 		for poqty in po_data:
 			if row.get('item_code') == poqty.item_code:
-				row['po_qty']+=poqty.qty
-				supplier.append(poqty.supplier)
-				row['supplier']=(list(set(supplier)))
+				row['po_qty']+=round(poqty.qty, 3)
+				supplier.append({'supplier':poqty.supplier, 'qty':poqty.qty, 'item_code':poqty.item_code})
+	
+	outputList=[]
+	for i,g in itertools.groupby(supplier, key=operator.itemgetter("item_code")):
+		outputList.append(list(g))
+	
+	for row in new_data:
+		for out in outputList:
+			if row.get('item_code') == out[0].get('item_code'):
+				values_by_supplier = {}
+				for d in out:
+					values_by_supplier.setdefault(d['supplier'], []).append(int(d['qty']))
+				supplier_data=[{'supplier':k, 'qty':sum(v)} for k, v in values_by_supplier.items()]
+				row['supplier']=supplier_data
+
 		row['consider_po_qty']=(row.get('current_stock')+row.get('po_qty'))-row.get('planing_qty')
 		row['no_consider_po_qty']=row.get('current_stock')-row.get('planing_qty')
+
+
+	#Production plan qty calculation
+	pp_item = []
+	for row in unic_bom:
+		pp_data = frappe.db.sql("""SELECT c.item_code, c.quantity from `tabProduction Plan` a join `tabProduction Plan Item` b on a.name=b.parent join `tabMaterial Request Plan Item` c on a.name=c.parent where b.bom_no='{0}' and a.docstatus=1 and (a.status='Submitted' or a.status='In Process') and a.posting_date>'{1}' and posting_date<'{2}'""".format(row, planning_master[0].get('from_date'), from_date), as_dict=1)
+		for row in pp_data:
+			pp_item.append(row)
+
+	#group_by pp qty base on item_code
+	values_by_item = {}
+	for d in pp_item:
+		values_by_item.setdefault(d['item_code'], []).append(int(d['quantity']))
+	pp_data=[{'item_code':k, 'qty':sum(v)} for k, v in values_by_item.items()]
+
+	for row in new_data:
+		for pp in pp_data:
+			if row.get('item_code') == pp.get('item_code'):
+				row['consider_po_qty'] = round((row.get('consider_po_qty')-pp.get('qty')),3)
+				row['no_consider_po_qty'] = round((row.get('no_consider_po_qty')-pp.get('qty')),3)
+
 
 	path = 'medtech_bpa/medtech_bpa/page/supplier_wise_rm_wis/supplier_wise_rm_wis.html'
 	html=frappe.render_template(path,{'data':new_data})
@@ -85,21 +133,3 @@ def get_filters_codition(filters):
 	if filters.get("planning_master"):
 		conditions += " and name = '{0}'".format(filters.get('planning_master'))
 	return conditions
-
-
-def get_child_boms(bom_item,bom_childs):
-	if bom_item.get("item_code") and bom_item.get("bom_no"):
-		bom_name = frappe.db.get_value("BOM",{'item':bom_item.get("item_code")},"name")
-		if bom_name:
-
-			childs = frappe.get_all("BOM Item",{"parent":bom_name},["item_code","parent", "item_name", 'bom_no', 'qty'])
-
-			for row in childs:
-				row['planning_master_item'] = bom_item.planning_master_item
-
-			bom_childs += childs
-
-			if len(childs)>0:
-				for c in childs:
-					get_child_boms(c,bom_childs)
-	return bom_childs
