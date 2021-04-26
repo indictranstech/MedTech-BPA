c14c1606cd8e95036667b19a0d0fe0394832ff02
namrataingale <namrata.i@indictrans.in>
2021-04-26 13:01:38 +0530

    [Fix][Sales Invoice , Supplier wise and RM wise report]

--- medtech_bpa/medtech_bpa/custom_scripts/sales_invoice/sales_invoice.py
+++ medtech_bpa/medtech_bpa/custom_scripts/sales_invoice/sales_invoice.py
@@ -3,18 +3,21 @@ import frappe
 
 
 def validate(doc, method):
-	so_name = [row.sales_order for row in doc.items if row.sales_order]
+	# so_name = [row.sales_order for row in doc.items if row.sales_order]
+	so_name = frappe.db.get_value("Sales Invoice Item",{'parent':doc.name},'sales_order')
 	if so_name:
-		so_doc =frappe.get_doc("Sales Order", so_name[0])
+		so_doc =frappe.get_doc("Sales Order", so_name)
 		so_doc.workflow_state = "Pending for Bill"
 		so_doc.db_update()
 		frappe.db.commit()
 
 
 def on_submit(doc, method):
-	so_name = [row.sales_order for row in doc.items]
+	# so_name = [row.sales_order for row in doc.items]
+	so_name = frappe.db.get_value("Sales Invoice Item",{'parent':doc.name},'sales_order')
+
 	if so_name:
-		so_doc =frappe.get_doc("Sales Order", so_name[0])
+		so_doc =frappe.get_doc("Sales Order", so_name)
 		so_doc.workflow_state = "Completed"
 		so_doc.db_update()
 		frappe.db.commit()
